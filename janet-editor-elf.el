2f20ccd9c3589e498136dd91509772dbffa7198c
sogaiu <983021772@users.noreply.github.com>
2021-12-12 08:09:19 +0900

    Add menu

--- janet-editor-elf.el
+++ janet-editor-elf.el
@@ -26,6 +26,8 @@
 ;;                (require 'janet-editor-elf)
 ;;                (setq-local indent-line-function
 ;;                            #'jee-indent-line)))
+;;
+;;    (add-hook 'janet-mode-hook #'jee-mode)
 
 ;;; Code:
 
@@ -33,6 +35,48 @@
 (require 'jee-indent-line)
 (require 'jee-indent-region)
 (require 'jee-tracev)
+(require 'jee-utils)
+
+(defvar jee-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "\C-c\C-t" 'jee-tracev-wrap)
+    (define-key map "\C-c\C-y" 'jee-tracev-unwrap)
+    (define-key map "\C-c\C-c" 'jee-comment-wrap)
+    (define-key map "\C-c\C-x" 'jee-comment-wrap-region)
+    (define-key map "\C-c\C-v" 'jee-comment-unwrap)
+    (easy-menu-define jee-mode-map map
+      "Janet Editor Elf Mode Menu"
+      '("Jee"
+        ["Wrap with tracev" jee-tracev-wrap t]
+        ["Unwrap tracev" jee-tracev-unwrap t]
+        "--"
+        ["Wrap with comment" jee-comment-wrap t]
+        ["Wrap region with comment" jee-comment-wrap-region t]
+        ["Unwrap comment" jee-comment-unwrap t]))
+    map)
+  "Janet editor elf mode map.")
+
+;;;###autoload
+(define-minor-mode jee-mode
+  "Minor mode for janet-editor-elf.
+
+The following keys are available in `jee-mode`:
+
+\\{jee-mode-mode}"
+
+  nil " jee" jee-mode-map)
+
+(jee-comment
+
+ (add-hook 'janet-mode-hook
+           (lambda ()
+             (require 'janet-editor-elf)
+             (setq-local indent-line-function
+                         #'jee-indent-line)))
+
+ (add-hook 'janet-mode-hook #'jee-mode)
+
+ )
 
 (provide 'janet-editor-elf)
 ;;; janet-editor-elf.el ends here
